b5fadc0fce03dfc610f86234d4411ac7a10251eb
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-09 12:54:42 +0530

    program 2

--- DayFive.java
+++ DayFive.java
@@ -1,13 +1,21 @@
+ 
+//program to add n natural numbers
 import java.util.*;
 public class DayFive{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum=0;
-        for (int i=0; i<=n; i++) {
-            sum=sum+i;     //Adding 0+i        
+       
+
+        for (int i=1; i<=10; i++) {
+           
+            System.out.print(n); 
+            System.out.print("*");
+            System.out.print(i);
+            System.out.print("=");
+            System.out.println(n*i);     //Adding 0+i        
         }
-        System.out.println(sum);
+       
 
     }
 }
