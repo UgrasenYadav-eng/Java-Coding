fa7c233fffc072c479d1b483da65e5ab3b67cfcf
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-10 19:16:09 +0530

    traingal revision

--- DaySix.java
+++ DaySix.java
@@ -1,25 +1,19 @@
 
-import java.util.Scanner;
-
 public class DaySix {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
       
         
-        int n = 5;
-       
-        //outer loop to print column
-        for (int i=1; i<=n; i++){
-            for (int j = 1; j <=n; j++) {
-                if (i==1||j==1||i==n||j==n) {
-                    System.out.print("*");             
-                } 
-                else {System.out.print(" ");
-                }             
-            }
-                        System.out.println();
+      int n = 4;
+       for (int i = 0; i <n ; i++) {
+        for (int j = 0; j <=i ; j++) {
+            
+            System.out.print("*");
         }
-        sc.close();
+           System.out.println();
+       }
+
+        }
+       
     }
     
-}
