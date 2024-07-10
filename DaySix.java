b78e7664052192ed373178a8c9ce08112d0c1981
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-10 17:42:16 +0530

    reverse solid triangle by stars

--- DaySix.java
+++ DaySix.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class DaySix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+      
+        int n = 4;
         
-        for (int i=0; i<=m-1; i++){
-            for (int j = 1; j <=i; j++) { 
+        for (int i=n; i>=1; i--){
+            for (int j = 2; j <=i; j++) { 
                     System.out.print("*");             
             }
             System.out.println("*");
