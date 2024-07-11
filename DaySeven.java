c55e24508babe8ee7b830e99a891db2380522667
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-11 22:38:32 +0530

    patern parimad

--- DaySeven.java
+++ DaySeven.java
@@ -4,13 +4,15 @@ public class DaySeven {
         int u = 5;
         for (int i = 1; i <=u; i++) {
             for (int j = 0; j <= u-i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
                 
             }
-            for (int j = 1; j <=i; j++) {
-                System.out.print(i+" ");                
+            for (int j = i; j >=1; j--) {
+                System.out.print(j+" ");                
             }
-           
+            for (int j = 2; j <=i; j++) {
+                System.out.print(j+" ");                
+            }
             System.out.println();
             
         }
