2a7d81453a947ebb3c1a7cebbf4f41e67e9f66a4
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-11 22:23:37 +0530

    numbers solid pjarimid pattern pronting

--- DaySeven.java
+++ DaySeven.java
@@ -3,14 +3,14 @@ public class DaySeven {
     public static void main(String[] args) {
         int u = 5;
         for (int i = 1; i <=u; i++) {
-            for (int j = 0; j <=u-i; j++) {
-                System.out.print("  ");
+            for (int j = 0; j <= u-i; j++) {
+                System.out.print(" ");
                 
             }
-            for (int j = 1; j <=u; j++) {
-                System.out.print(" *");
-                
+            for (int j = 1; j <=i; j++) {
+                System.out.print(i+" ");                
             }
+           
             System.out.println();
             
         }
