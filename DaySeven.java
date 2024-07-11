b3016f838a23318642943f76d8ba445a6976fb42
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-11 23:19:31 +0530

    barfi pattern by soid stars

--- DaySeven.java
+++ DaySeven.java
@@ -3,18 +3,39 @@ public class DaySeven {
     public static void main(String[] args) {
         int u = 5;
         for (int i = 1; i <=u; i++) {
+            //1st half start from here
             for (int j = 0; j <= u-i; j++) {
                 System.out.print("  ");
                 
             }
-            for (int j = i; j >=1; j--) {
-                System.out.print(j+" ");                
+            
+            for (int j = 1; j <=i; j++) {
+                System.out.print(" *");                
             }
             for (int j = 2; j <=i; j++) {
-                System.out.print(j+" ");                
+                System.out.print(" *");                
+            }        
+            System.out.println();     
+            //1st half ok and done.
+
+            // 2nd half   
+            
             }
-            System.out.println();
+            for (int i = 0; i <= u; i++) {
+                for (int j = 0; j <=i; j++) {
+                    System.out.print("  ");
+                    
+                }
+                for (int j = 0; j < u-i; j++) {
+                    System.out.print(" *");                    
+                }
+                for (int j = 1; j < u-i; j++) {
+                    System.out.print(" *");                    
+                }
+                System.out.println();
+                    
+                }
+        
             
         }
     }
-}
