fe7902ff3c9e570937933175fb07172fad58f7bc
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-11 22:18:12 +0530

    patter printion foot steps slider type pateern

--- DaySeven.java
+++ DaySeven.java
@@ -2,28 +2,12 @@ public class DaySeven {
 
     public static void main(String[] args) {
         int u = 5;
-        for (int i = 0; i <=u; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");                
+        for (int i = 1; i <=u; i++) {
+            for (int j = 0; j <=u-i; j++) {
+                System.out.print("  ");
+                
             }
-            int star= u - i;
-            for (int j = 1; j <= star; j++) {
-                System.out.print("    ");                
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(" *");                
-            }
-            
-            System.out.println();       
-        }
-        for (int i = 0; i <=u; i++) {
-            for (int j = 1; j <= u-i; j++) {
-                System.out.print("* ");                
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("    ");                
-            }
-            for (int j = 1; j <=u-i; j++) {
+            for (int j = 1; j <=u; j++) {
                 System.out.print(" *");
                 
             }
