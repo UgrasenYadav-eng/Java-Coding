58cee33b0ad6ab412672a548e26c4f718f4d4d0d
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-12 13:34:26 +0530

    Multiplication by java function revision java

--- DayEight.java
+++ DayEight.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 public class DayEight {
 
-    public static int addTwoNum(int a, int b){
-    int sum = a + b;
-    return sum;
+    public static int mulOfTwoNum(int a, int b){
+        int mul = a * b;
+        return mul;
     }
+    //main function start from here
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int sum= addTwoNum(a, b);
-        System.out.println("Addition of two numbers are =" + sum);
+        // calling the function
+        int mul = mulOfTwoNum(a, b);
+        System.out.println("Multiplication of the two numbers are :" + mul);
     }
-    }
+}
