07c9665a8f94c7c005e8ae1908794a90c0538863
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-12 17:41:18 +0530

    circumference of a circle by using function   java revision

--- DayEight.java
+++ DayEight.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 public class DayEight {
 
-    public static int greaterOfTwo(int a, int b){
+    public static double  cricleCrecumfrance(double a ){
         // function for checking which one is greater in both the given nembers
-        if (a > b) {return a;            
-        }
-        else{return b;}
+        double cr = 2 * (3.14) * a;
+        return cr;
     }
     //main function will start from here
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the two input nembers");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        System.out.println("Enter the input nember");
+        double a = sc.nextInt();
+        
 
-        int Greater = greaterOfTwo(a, b);
-        System.out.println(Greater + " is the greater of these two inputs");
+        double cr = cricleCrecumfrance(a);
+        System.out.println(cr + " is the Circumference of the circle");
     }
 }
