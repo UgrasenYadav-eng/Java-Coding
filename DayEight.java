f0db55a9e988f86c732fc32558d45d540fbfd507
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-12 17:59:49 +0530

    infinite loop java function by using do while   java revision

--- DayEight.java
+++ DayEight.java
@@ -1,19 +1,13 @@
-import java.util.Scanner;
 public class DayEight {
 
-    public static double  cricleCrecumfrance(double a ){
+    public static void infiniteLoop( ){
         // function for checking which one is greater in both the given nembers
-        double cr = 2 * (3.14) * a;
-        return cr;
+        do {
+            System.out.println("this is an infinite loop press ctrl+C to stop this");
+        } while (true);
     }
     //main function will start from here
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the input nember");
-        double a = sc.nextInt();
-        
-
-        double cr = cricleCrecumfrance(a);
-        System.out.println(cr + " is the Circumference of the circle");
-    }
+        infiniteLoop();
+     }
 }
