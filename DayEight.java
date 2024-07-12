28a790cddd78af6471997cee6bc9e56125bd547c
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-12 12:51:29 +0530

    java function 1st program revision

--- DayEight.java
+++ DayEight.java
@@ -1,18 +1,15 @@
+
+import java.util.Scanner;
+
 public class DayEight {
-    
+
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
     public static void main(String[] args) {
-        int u= 5;
-        for (int i = 0; i <=u; i++) {
-            for (int j = 0; j <=i; j++) {
-                System.out.print("*");                
-            }
-            for (int j = 1; j <=2*(u-i); j++) {
-                System.out.print(" ");                
-            }
-            for (int j = 0; j <=i; j++) {
-                System.out.print("*");                
-            }
-            System.out.println();
-        }
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
     }
 }
