158ef4ac28b8015e828433bfce2340a7d091f4f7
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-13 14:03:32 +0530

    java array revision 1

---
+++ DayNine.java
@@ -0,0 +1,21 @@
+public class DayNine {
+    public static void main(String args[]){
+        int marks [] =new int[3];
+        // int [] marks = new int[3];
+         marks [0]= 97;
+         marks [1]= 98;
+         marks [2]= 92;
+
+     /*  System.out.println(marks[0]);
+        System.out.println(marks[0]);
+        System.out.println(marks[0]);
+    */
+                    // OR
+    
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);            
+        }           
+    }
+
+    
+}
