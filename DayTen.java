ed7f6137f0cb87beba71131244e076a8b7ba5a80
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-14 13:02:29 +0530

    comapreing to strings  java revision [ fullName.CharAt(i) ]

--- DayTen.java
+++ DayTen.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 public class DayTen{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the name here");
-        // statment to take String input
-        String name = sc.nextLine();
+
+        // taking 1st input
+        System.out.println("enter the name 1");
+        String name1 = sc.nextLine();
+
+         // taking 2nd input
+        System.out.println("enter the name 2");
+        String name2 = sc.nextLine();
         
-        System.out.println(name);
+        // comparing the both names
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("both nem are equals");            
+        } else { System.out.println("numbers are not equals");}
+        
         // closing Scanner class  
         sc.close();
     }
