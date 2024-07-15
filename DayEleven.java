e5de05c8cd7b83e00c7b7eaa0e97997fd4e6751a
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-15 23:38:43 +0530

    bit mask "Set" int Uggu= bitMask | a;

--- DayEleven.java
+++ DayEleven.java
@@ -3,10 +3,9 @@ public class DayEleven {
         int a = 5;
         int pos = 2;
         int bitMask= 1<<pos;
-        if ((bitMask & a) == 0) {
-            System.out.println("Bit was 0");            
-        }
-        else{ System.out.println("Bit was 1");}
+      
+      int Uggu = bitMask | a;
+        System.out.println(Uggu);            
+        
     }
-    
 }
